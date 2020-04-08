20c44a825a225a5d1813ac1936a6b84111f5a54f
DmitriyChigrinskiy <dimkoo.arm@gmail.com>
2020-04-08 21:28:42 +0300

    Написал Hello World на C#

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Klass
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Hello World !");
+        }
+    }
+}
+
